6e3d00a6a0ce7a40b2dd0a7ea17452cd4a2c006f
vishal.srivastava <vishalsrivastava@deloitte.com>
2022-11-12 23:40:47 +0530

    reository added

--- spring/src/main/java/com/springboot/imaginnoavte/spring/employeeinterface/serviceimpl/EmployeeRepository.java
+++ spring/src/main/java/com/springboot/imaginnoavte/spring/employeeinterface/serviceimpl/EmployeeRepository.java
@@ -1,11 +1,13 @@
-package com.springboot.imaginnoavte.spring.repository;
+package com.springboot.imaginnoavte.spring.employeeinterface.serviceimpl;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springboot.imaginnoavte.spring.employeeDTO.EmployeeTable;
 import java.lang.String;
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeTable, String> {
 
 
